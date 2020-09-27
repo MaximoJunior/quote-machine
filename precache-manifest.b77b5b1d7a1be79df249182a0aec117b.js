57034c826e45e1c9482d97c8418f37b853c13131
MaximoJunior <studentdeveloper19@gmail.com>
2020-09-27 09:14:52 -0400

    Updates

---
+++ precache-manifest.b77b5b1d7a1be79df249182a0aec117b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61c1f2fd0e88cb51449b31680f4de1a1",
+    "url": "/quote-machine/index.html"
+  },
+  {
+    "revision": "a4a5c0afaab1c26b2f88",
+    "url": "/quote-machine/static/css/main.dcf57ad3.chunk.css"
+  },
+  {
+    "revision": "5ac636e55c1cbe7ac493",
+    "url": "/quote-machine/static/js/2.82eb577a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quote-machine/static/js/2.82eb577a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4a5c0afaab1c26b2f88",
+    "url": "/quote-machine/static/js/main.6eae8e31.chunk.js"
+  },
+  {
+    "revision": "6c1232aa5663ceebc4f7",
+    "url": "/quote-machine/static/js/runtime-main.f2084995.js"
+  }
+]);
